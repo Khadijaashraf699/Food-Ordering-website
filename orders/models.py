75add7445d29e306d50017fb6de104b6e1069500
khadijaashraf <khadijaashraf699@gmail.com>
2025-08-28 14:55:24 +0500

    Added accounts app, updated models, templates and static files

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to= '/media/')
+    def __str__(self):
+        return self.name
+class Sizes(models.TextChoices):
+    SMALL = 'S', 'Small'
+    LARGE = 'L', 'Large'
+    EXTRA_LARGE = 'XL', 'Extra Large'
+    
+class FoodItems(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.CharField(max_length=100, null=True, blank=True)
+    size = models.CharField(max_length=10, null=True, blank=True, choices=Sizes)
+    price = models.DecimalField(blank=False)
+    
+    
+class Image(models.Model):
+    name = models.CharField(primary_key=True)
+    image = models.ImageField(upload_to='/media/')
+
+
+
+
+
+
+
+
+
+
+
